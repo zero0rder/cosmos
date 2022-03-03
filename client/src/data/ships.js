4e5546776439d7ced3302ae5c739c1b3905f9ec9
Dwight Smith <dwightsmth@gmail.com>
2022-03-02 23:40:37 -0500

    ships and planet meta data

---
+++ client/src/data/ships.js
@@ -0,0 +1,7 @@
+export default [
+    {name: 'Falcon 9', provider: 'spaceX'}, 
+    {name: 'Falcon 1', provider: 'spaceX'}, 
+    {name: 'Saturn V', provider: 'spaceX'},
+    {name: 'New Shepard', provider: 'blueOrigin'}, 
+    {name: 'New Glenn', provider: 'blueOrigin'}
+]
