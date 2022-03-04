a57ae0a0dfd681129bfaeac47770ce757b03bdbf
Dwight Smith <dwightsmth@gmail.com>
2022-03-03 22:56:30 -0500

    planets header/data update

--- client/src/components/galaxy/planets/planets.js
+++ client/src/components/galaxy/planets/planets.js
@@ -4,6 +4,9 @@ import planetData from '../../../data/planets';
 const Planets = () => {
     return (
         <section className='planets'>
+            <header>
+                <span>Destinations</span>
+            </header>
             { planetData.map((e, i) => <Planet key={i} planet={e} /> )}
         </section>
     );
