1301bf5dd0cec8b45881f31c496c9ca597c1b252
Dwight Smith <dwightsmth@gmail.com>
2022-03-06 22:28:23 -0500

    added confirmation page

---
+++ client/src/components/pages/confirmation.js
@@ -0,0 +1,51 @@
+import { useLocation } from "react-router-dom";
+
+const Confirmation = () => {
+    let { state: {formData, planet} } = useLocation();
+
+    return (
+        <section className='confirmation-page'>
+            <section className='confirmation-section'>
+                <p>Hello { formData.firstname }, below is confirmation for your upcoming trip to { planet }!</p>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>
+                                    <span>Confirmation Details:</span>
+                                </td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Passenger:</td>
+                                <td>{ formData.firstname + ' ' +  formData.lastname }</td>
+                            </tr>
+                            <tr>
+                                <td>Ship:</td>
+                                <td>{ formData.ship }</td>
+                            </tr>
+                            <tr>
+                                <td>Destination:</td>
+                                <td>{ planet }</td>
+                            </tr>
+                            <tr>
+                                <td>Notes:</td>
+                                <td>{ formData.message }</td>
+                            </tr>
+                            <tr>
+                                <td>Email:</td>
+                                <td>{ formData.email }</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+            <div className="goto-account-btn">
+                <button>Go to Account</button>
+            </div>
+        </section>
+    );
+}
+
+export default Confirmation;
