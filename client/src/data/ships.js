ec191625e8a6fd48eb41714aed1188661282672c
Dwight Smith <dwightsmth@gmail.com>
2022-10-09 23:12:19 -0400

    delete old files

--- client/src/data/ships.js
+++
@@ -1,7 +0,0 @@
-export default [
-    {name: 'Falcon 9', provider: 'spaceX'}, 
-    {name: 'Falcon 1', provider: 'spaceX'}, 
-    {name: 'Saturn V', provider: 'spaceX'},
-    {name: 'New Shepard', provider: 'blueOrigin'}, 
-    {name: 'New Glenn', provider: 'blueOrigin'}
-]
